ac16e23f6bc3d5748fe797cd8a96065cc46b909f
martin123154 <martin123154@gmail.com>
2015-12-13 20:18:23 +0100

    dodanie min i max dlugosci

--- src/main/java/com/pgs/soft/visit/validation/PhoneValidator.java
+++ src/main/java/com/pgs/soft/visit/validation/PhoneValidator.java
@@ -14,14 +14,14 @@ public class PhoneValidator implements ConstraintValidator<Phone, String> {
         if(phoneNo == null){
             return true;
         }
-        if (phoneNo.matches("\\d{9}")) return true;
-        else if (phoneNo.matches("\\d{7}")) return true;
-        else if(phoneNo.matches("\\d{3}[-\\.\\s]\\d{3}[-\\.\\s]\\d{3}")) return true;
-        else if(phoneNo.matches("\\d{3}-\\d{3}-\\d{3}\\s(x|(ext))\\d{3,5}")) return true;
-        else if(phoneNo.matches("\\(\\d{3}\\)-\\d{3}-\\d{3}")) return true;
-        else if(phoneNo.matches("\\d{3}[-\\.\\s]\\d{2}[-\\.\\s]\\d{2}")) return true;
-        else if(phoneNo.matches("\\d{3}-\\d{2}-\\d{2}\\s(x|(ext))\\d{3,5}")) return true;
-        else if(phoneNo.matches("\\(\\d{3}\\)-\\d{2}-\\d{2}")) return true;
+        if (phoneNo.matches("^(?=.{2,12}$)\\d[0-9]*")) return true;
+       // else if (phoneNo.matches("\\d{7}")) return true;
+     //   else if(phoneNo.matches("\\d{3}[-\\.\\s]\\d{3}[-\\.\\s]\\d{3}")) return true;
+      //  else if(phoneNo.matches("\\d{3}-\\d{3}-\\d{3}\\s(x|(ext))\\d{3,5}")) return true;
+     //   else if(phoneNo.matches("\\(\\d{3}\\)-\\d{3}-\\d{3}")) return true;
+     //   else if(phoneNo.matches("\\d{3}[-\\.\\s]\\d{2}[-\\.\\s]\\d{2}")) return true;
+     //   else if(phoneNo.matches("\\d{3}-\\d{2}-\\d{2}\\s(x|(ext))\\d{3,5}")) return true;
+     //   else if(phoneNo.matches("\\(\\d{3}\\)-\\d{2}-\\d{2}")) return true;
      
         else return false;
     }
