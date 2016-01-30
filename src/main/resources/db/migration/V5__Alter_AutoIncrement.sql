4615379444343cf4d4c875aa47bfdb9269d4e59c
Amenorphus <robert779797@gmail.com>
2016-01-30 13:16:36 +0100

    ustalone nazwy kluczy obcych

--- src/main/resources/db/migration/V5__Alter_AutoIncrement.sql
+++ src/main/resources/db/migration/V5__Alter_AutoIncrement.sql
@@ -1,8 +1,8 @@
-ALTER TABLE visit.employee DROP FOREIGN KEY employee_ibfk_1;
-ALTER TABLE visit.user DROP FOREIGN KEY user_ibfk_1;
-ALTER TABLE visit.visit DROP FOREIGN KEY visit_ibfk_1;
-ALTER TABLE visit.visit DROP FOREIGN KEY visit_ibfk_2;
-ALTER TABLE visit.schedule DROP FOREIGN KEY schedule_ibfk_1;
+ALTER TABLE visit.employee DROP FOREIGN KEY employee_outpost;
+ALTER TABLE visit.user DROP FOREIGN KEY user_employee;
+ALTER TABLE visit.visit DROP FOREIGN KEY visit_employee;
+ALTER TABLE visit.visit DROP FOREIGN KEY visit_customer;
+ALTER TABLE visit.schedule DROP FOREIGN KEY schedule_employee;
 
 ALTER TABLE visit.outpost MODIFY id BIGINT(6) AUTO_INCREMENT;
 ALTER TABLE visit.employee MODIFY id BIGINT(6) AUTO_INCREMENT;
@@ -11,8 +11,8 @@ ALTER TABLE visit.visit MODIFY id BIGINT(6) AUTO_INCREMENT;
 ALTER TABLE visit.schedule MODIFY id BIGINT(6) AUTO_INCREMENT;
 ALTER TABLE visit.user MODIFY id BIGINT(6) AUTO_INCREMENT;
 
-ALTER TABLE visit.employee ADD FOREIGN KEY (idOutpost) REFERENCES visit.outpost(id);
-ALTER TABLE visit.user ADD FOREIGN KEY (idEmployee) REFERENCES visit.employee(id);
-ALTER TABLE visit.visit  ADD FOREIGN KEY (idEmployee) REFERENCES visit.employee(id);
-ALTER TABLE visit.visit ADD FOREIGN KEY (idCustomer) REFERENCES visit.customer(id);
-ALTER TABLE visit.schedule ADD FOREIGN KEY (idEmployee) REFERENCES visit.employee(id);
+ALTER TABLE visit.employee ADD CONSTRAINT employee_outpost FOREIGN KEY (idOutpost) REFERENCES visit.outpost(id);
+ALTER TABLE visit.user ADD CONSTRAINT user_employee FOREIGN KEY (idEmployee) REFERENCES visit.employee(id);
+ALTER TABLE visit.visit  ADD CONSTRAINT visit_employee FOREIGN KEY (idEmployee) REFERENCES visit.employee(id);
+ALTER TABLE visit.visit ADD CONSTRAINT visit_customer FOREIGN KEY (idCustomer) REFERENCES visit.customer(id);
+ALTER TABLE visit.schedule ADD CONSTRAINT schedule_employee FOREIGN KEY (idEmployee) REFERENCES visit.employee(id);
