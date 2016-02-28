cf0a2ccbdc49712aa9271756afc4de613299c9e6
Amenorphus <robert779797@gmail.com>
2016-02-28 20:52:25 +0100

    config context

--- src/test/java/com/pgs/soft/visit/service/ScheduleDTOServiceTest.java
+++ src/test/java/com/pgs/soft/visit/service/ScheduleDTOServiceTest.java
@@ -6,13 +6,26 @@ import java.util.Date;
 
 import org.joda.time.DateTime;
 import org.junit.Test;
-
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.support.AnnotationConfigContextLoader;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.pgs.soft.visit.init.WebAppConfig;
 
 import com.pgs.soft.visit.domain.Schedule;
 import com.pgs.soft.visit.dto.ScheduleDTO;
 
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes=WebAppConfig.class)
+//@ContextConfiguration(classes=WebAppConfig.class, loader=AnnotationConfigContextLoader.class)
+//@Transactional
 public class ScheduleDTOServiceTest {
+
 	
 	@Autowired
 	ScheduleDTOService scheduleDTOService;
