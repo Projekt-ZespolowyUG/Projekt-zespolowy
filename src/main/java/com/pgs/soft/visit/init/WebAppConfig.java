b94069c1cb3cfb5339144fdd20c0bd9c93ec22f5
martin123154 <martin123154@gmail.com>
2016-04-17 13:19:42 +0200

    merge rejestracja/logowanie

--- src/main/java/com/pgs/soft/visit/init/WebAppConfig.java
+++ src/main/java/com/pgs/soft/visit/init/WebAppConfig.java
@@ -15,6 +15,7 @@ import org.springframework.core.env.Environment;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.orm.hibernate5.HibernateTransactionManager;
 import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
+import org.springframework.security.access.SecurityConfig;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
